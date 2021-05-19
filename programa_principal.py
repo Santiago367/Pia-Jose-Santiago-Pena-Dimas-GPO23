c60f0df58b9a61a6eccdfbdf8647c45efdd8c309
Santiago367 <saantiago367@gmail.com>
2021-05-19 17:52:48 -0500

    menu para agregar empleado

--- programa_principal.py
+++ programa_principal.py
@@ -6,6 +6,36 @@ idej = "12"
 nomej = "lolo"
 direj = "democr"
 
+while not salir:
+ 
+    print ("1. Administrar Empleados")
+    print ("7. Salir")
+
+    print ("Elige una opcion")
+    opcion = input()
+ 
+    if opcion == 1:
+        print("Empleados")
+        print("1. Registrar Empleado")
+        print("2. Mostrar todos los empleados")
+        print("3. Buscar empleado por id")
+
+        opcion_empleados = input()
+        if opcion_empleados == 1:
+            print("ingrese el id del Empleado: ")
+            id_del_empleado = input()
+            print("Ingrese el nombre del Empleado: ")
+            nombre_del_empleado = input()
+            print("Ingrese la direccion del Empleado: ")
+            direccion_del_empleado = input()
+
+            crear_empleado = Empleado(id_del_empleado,nombre_del_empleado,direccion_del_empleado)
+            crear_empleado.guardar()
+    elif opcion == 7:
+        salir = True
+    else:
+        print ("Introduce un numero entre 1 y 3")
+
 crear_empleado = Empleado(idej,nomej,direj)
 
 crear_empleado.guardar()
