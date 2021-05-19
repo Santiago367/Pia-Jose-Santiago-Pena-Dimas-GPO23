4df32f357ad432d5f290484a1e50cbb06d2e7668
Santiago367 <saantiago367@gmail.com>
2021-05-19 17:39:42 -0500

    comienzo el menu

---
+++ programa_principal.py
@@ -0,0 +1,12 @@
+from empleado import Empleado
+
+empleado3 = Empleado("5","ejemplo4","Avenida23")
+
+idej = "12"
+nomej = "lolo"
+direj = "democr"
+
+crear_empleado = Empleado(idej,nomej,direj)
+
+crear_empleado.guardar()
+Empleado.consultar_por_id(Empleado, "5")
