2594e2445ab70c21e507e76a54c701b488d8589f
Santiago367 <saantiago367@gmail.com>
2021-05-19 18:51:17 -0500

    agregado menu para cursos

--- programa_principal.py
+++ programa_principal.py
@@ -1,11 +1,13 @@
 from empleado import Empleado
+from curso import Curso
 
 salir = False
 
 while not salir:
  
-    print ("1. Administrar Empleados")
-    print ("7. Salir")
+    print("1. Administrar Empleados")
+    print("2. Administrar Cursos")
+    print("7. Salir")
 
     print ("Elige una opcion2")
     opcion = int(input())
@@ -37,7 +39,34 @@ while not salir:
             Empleado.consultar_por_id(Empleado, id_empleado_buscar)
             break
         break
-    
+
+    elif opcion == 2:
+        print("Cursos")
+        print("1. Registrar curso")
+        print("2. Mostrar todos los cursos")
+        print("3. Buscar curso por id")
+
+        opcion_cursos = int(input())
+
+        if opcion_cursos == 1:
+            print("ingrese el id del curso: ")
+            id_del_curso = input()
+            print("Ingrese la descripcion del curso: ")
+            descripcion_del_curso = input()
+            print("Ingrese la id del Empleado: ")
+            id_del_empleado = input()
+
+            crear_curso = Curso(id_del_curso,descripcion_del_curso,id_del_empleado)
+            crear_curso.guardar()
+            break
+        elif opcion_cursos == 2:
+            Curso.consultar_todo()
+            break
+        elif opcion_cursos == 3:
+            print("¿Cual es la id que desea buscar?")
+            id_curso_buscar = input()
+            Curso.consultar_por_id(Curso, id_curso_buscar)
+            break
     elif opcion == 7:
         salir = True
         break
