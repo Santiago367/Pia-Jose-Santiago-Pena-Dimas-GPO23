8bff966c19ea0307fe6cef9306f3dac9314de6a8
Santiago367 <saantiago367@gmail.com>
2021-05-19 13:47:24 -0500

    creado inits en pv empleado

---
+++ empleado.py
@@ -0,0 +1,19 @@
+class Empleado:
+    def __init__(self, id_empleado = 1, nombre = "fsfs", direccion = ""):
+        self.__id_empleado = id_empleado
+        self.__nombre = nombre
+        self.__direccion = direccion
+    
+    @property
+    def id_empleado(self):
+        return self.__id_empleado
+    
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @property
+    def direccion(self):
+        return self.__direccion
+
+
