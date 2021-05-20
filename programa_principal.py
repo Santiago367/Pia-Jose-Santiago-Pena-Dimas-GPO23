017f350a354f6ff1273862d714ae49b9c6adfea6
Santiago367 <saantiago367@gmail.com>
2021-05-19 19:55:28 -0500

    creado menu para temas en principal

--- programa_principal.py
+++ programa_principal.py
@@ -1,6 +1,7 @@
 from empleado import Empleado
 from curso import Curso
 from video import Video
+from tema import Tema
 
 def agregar_empleado():
     print("ingrese el id del Empleado: ")
@@ -37,6 +38,15 @@ def agregar_video():
     crear_video = Video(id_del_video,nombre_del_video,url_del_video,fechapublicacion_del_video)
     crear_video.guardar()
 
+def agregar_tema():
+    print("ingrese el id del tema: ")
+    id_del_tema = input()
+    print("Ingrese el nombre del tema: ")
+    nombre_del_tema = input()
+
+    crear_tema = Tema(id_del_tema,nombre_del_tema)
+    crear_tema.guardar()
+
 salir = False
 
 while not salir:
@@ -62,7 +72,7 @@ while not salir:
             Empleado.consultar_todo()
             break
         elif opcion_empleados == 3:
-            print("¿Cual es la id que desea buscar?")
+            print("¿Cual es la id del empleado que desea buscar?")
             id_empleado_buscar = input()
             Empleado.consultar_por_id(Empleado, id_empleado_buscar)
             break
@@ -83,7 +93,7 @@ while not salir:
             Curso.consultar_todo()
             break
         elif opcion_cursos == 3:
-            print("¿Cual es la id que desea buscar?")
+            print("¿Cual es la id del curso que desea buscar?")
             id_curso_buscar = input()
             Curso.consultar_por_id(Curso, id_curso_buscar)
             break
@@ -103,9 +113,29 @@ while not salir:
             Video.consultar_todo()
             break
         elif opcion_cursos == 3:
-            print("¿Cual es la id que desea buscar?")
+            print("¿Cual es la id del video que desea buscar?")
             id_video_buscar = input()
             Video.consultar_por_id(Video, id_video_buscar)
+            break
+
+    elif opcion == 4:
+        print("Temas")
+        print("1. Registrar tema")
+        print("2. Mostrar todos los temas")
+        print("3. Buscar tema por id")
+
+        opcion_temas = int(input())
+
+        if opcion_temas == 1:
+            agregar_tema()            
+            break
+        elif opcion_temas == 2:
+            Tema.consultar_todo()
+            break
+        elif opcion_temas == 3:
+            print("¿Cual es la id del tema que desea buscar?")
+            id_tema_buscar = input()
+            Video.consultar_por_id(Video, id_tema_buscar)
             break
     elif opcion == 7:
         salir = True
